98fc414854d105636aca9ed66396ab77ce37954d
ophicial-pauloski <pasukwo5@gmail.com>
2022-07-20 15:33:53 +0100

    updated features

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -2,6 +2,8 @@ import { MainTop } from '../mainContainer/MainTop';
 import {  Routes, Route } from "react-router-dom";
 import { PostCard } from '../mainContainer/PostCard';
 import { PostDetails } from './PostDetails';
+import { AllDiscussion } from './AllDiscussion';
+import { MyFavorite } from './MyFavorite';
 
 export const Home = () => {
   return (
@@ -16,6 +18,8 @@ export const Home = () => {
         }
       />
       <Route path='/post/:id' element={<PostDetails />} />
+      <Route path='/all-discusion' element={<AllDiscussion />} />
+      <Route path='/my-favorite' element={<MyFavorite />} />
     </Routes>
   );
 }
--- src/components/sidebar/SidebarMenu.tsx
+++ src/components/sidebar/SidebarMenu.tsx
@@ -1,15 +1,16 @@
-import { Button } from "react-bootstrap";
-import { openCreatePostModal } from "../../features/indexSlice";
+import { openCreatePostModal, openLoginModal } from "../../features/indexSlice";
 import "./sidebarMenu.css";
 import { useDispatch, useSelector } from "react-redux";
-import { CreatePostModal } from '../modals/CreatePostModal';
-import { useToast } from '@chakra-ui/react';
+import { CreatePostModal } from "../modals/CreatePostModal";
+import { useToast, Button, Box, Text } from "@chakra-ui/react";
 import { SideBarCategories } from "./SideBarCategories";
-import { Box } from '@chakra-ui/react';
-
+import { AiOutlineComment } from "react-icons/ai";
+import { BsFillStarFill } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
 export const SidebarMenu = () => {
   const toast = useToast();
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
   const { user } = useSelector((state: any) => state.auth);
@@ -24,24 +25,60 @@ export const SidebarMenu = () => {
         duration: 9000,
         isClosable: true,
       });
+  };
+
+  const openLoginScreen = () => {
+    dispatch(openLoginModal());
+  };
+
+  const allDiscusion = () => {
+    navigate("/all-discusion");
+  }
+  const myFavorite = () => {
+    navigate("/my-favorite");
   }
   return (
-    <section className='sidebar-menu'>
-      <Button className='my-5 p-3' onClick={handleCreatePost}>Start A New Conversion</Button>
-      <div className='menu1'>
-        <span>
-          <i className='fas fa-comments'></i>
-          All Discussions
-        </span>
-        <span>
-          <i className='fas fa-star'></i>
-          Starred
-        </span>
-      </div>
-      <hr />
-      <Box >
-        <SideBarCategories />
-      </Box>
+    <section className='sidebar-menu pe-3'>
+      {user ? (
+        <>
+          {" "}
+          <Button
+            bg='#0b87ff'
+            w={"100%"}
+            size={"md"}
+            my={5}
+            p={3}
+            _hover={{ bg: "#0b87ff" }}
+            disabled={user === null}
+            onClick={handleCreatePost}
+          >
+            <Text fontSize={"lg"}>Create Post</Text>
+          </Button>
+          {user && (
+            <div className='menu1'>
+              <Button bg={"none"} onClick={allDiscusion}>
+                <AiOutlineComment color='#0b87ff' size={20} />
+                <Text ml={2}>All Discussions</Text>
+              </Button>
+              <Button bg={"none"} mt={3} mb={5} onClick={myFavorite}>
+                <BsFillStarFill color='#0b87ff' size={20} />
+                <Text ml={2}>My Favorites</Text>
+              </Button>
+            </div>
+          )}
+          <hr />
+          <Box>
+            <SideBarCategories />
+          </Box>
+        </>
+      ) : (
+        <Box border={"1px"} w='100%' p={2} borderColor='#e5e5e5' mt={"150px"}>
+          <Text fontSize={"lg"}>You must be logged in to create a post</Text>
+          <Button display={'flex'} mx={'auto'} mt={2} bg='#0b87ff' w={'50%'} onClick={openLoginScreen}>
+            Login
+          </Button>
+        </Box>
+      )}
       <CreatePostModal />
     </section>
   );
